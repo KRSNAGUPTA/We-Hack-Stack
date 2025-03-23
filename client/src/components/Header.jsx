c7342ac3c353046f3953dd970d16dc558a487aca
KRSNAGUPTA <up.krishnagupta@gmail.com>
2025-03-23 11:12:07 +0530

    style: format Header component for improved code consistency

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,21 +3,22 @@ import { Dock, DockIcon } from "./magicui/dock";
 import { Bot, File, Home, Search } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { Separator } from "./ui/separator";
-import QuizIcon from '@mui/icons-material/Quiz';
+import QuizIcon from "@mui/icons-material/Quiz";
 function Header() {
   const navigate = useNavigate();
   return (
     <div>
       <Dock direction="middle">
         <DockIcon onClick={() => navigate("/")}>
-          <Home className="text-indigo-700"/>
+          <Home className="text-indigo-700" />
         </DockIcon>
         <Separator orientation="vertical" />
         <DockIcon onClick={() => navigate("/case")}>
           <Search />
         </DockIcon>
+        <Separator orientation="vertical" />
         <DockIcon onClick={() => navigate("/quiz")}>
-          <QuizIcon/>
+          <QuizIcon />
         </DockIcon>
         <Separator orientation="vertical" />
         <DockIcon onClick={() => navigate("/chat")}>
